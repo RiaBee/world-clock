eb1038004a8312c531eae74b8b3f4c7e2862d3b8
RiaBee <118854534+RiaBee@users.noreply.github.com>
2023-03-02 14:21:12 +0100

    moves all cities link to javascript

--- javascript/index.js
+++ javascript/index.js
@@ -44,15 +44,20 @@ function updateCity(event) {
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
-          <div>
-            <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )}<small>${cityTime.format("A")}<small></div>
-        </div>
-        `;
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">
+        ${cityTime.format("MMMM Do YYYY")}
+      </div>
+    </div>
+    <div class="time">
+      ${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}<small>
+    </div>
+  </div>
+  <div>
+    <a href="">All Cities</a>
+  </div>
+  `;
 }
 
 updateTime();
